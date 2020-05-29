b5367825787226a02c2da89f3f19e62ef7a44e4b
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-05-29 14:54:37 -0400

    Add a fetch function for both the header and the footer to be applied to every page.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,20 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+fetch("header.html")
+                .then(response => {
+                    return response.text();
+                })
+                .then(data => {
+                    document.querySelector("header").innerHTML = data;
+                });
+
+fetch("footer.html")
+                .then(response => {
+                    return response.text();
+                })
+                .then(data => {
+                    document.querySelector("footer").innerHTML = data;
+                });
+    
