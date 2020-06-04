44992fdd1c82267950f69cc6b2cfc780fc8b8479
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-06-04 19:20:22 +0000

    Add folder to hold my java class files.

---
+++ portfolio/src/main/java/com/google/sps/classes/Comment.java
@@ -0,0 +1,28 @@
+package com.google.sps.classes;
+
+import java.util.Date;
+
+public class Comment {
+  private String name;
+  private String message;
+  private Date time;
+
+  public Comment(String name, String message, Date time) {
+    this.name = name;
+    this.message = message;
+    this.time = time;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+
+  public Date getTime(){
+    return this.time;
+  }
+
+}
